07fdd13e4f4fef132ceba7753c4ecf3e7422df69
Sunny <sunnykumargupta9223@gmail.com>
2020-12-22 11:46:08 +0530

    Removed extra brackets

--- partials/_loginModal.php
+++ partials/_loginModal.php
@@ -7,7 +7,7 @@
                 <h5 class="modal-title" id="loginModalLabel">Login to our Forum</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
-                </button>>>
+                </button>
             </div>
             <div class="modal-body">
                 <form action="/forum/partials/_handleLogin.php" method="POST">
